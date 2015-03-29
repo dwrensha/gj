9a81d2d6e0d8d183d3e3a9e014fdd1f442e8aac1
David Renshaw <dwrenshaw@gmail.com>
2015-03-28 23:04:34 -0400

    Some basics.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,44 @@
 
 
+pub type Error = Box<::std::error::Error>;
+pub type Result<T> = ::std::result::Result<T, Error>;
+
 pub struct Promise<T> {
-    phatom_data : ::std::marker::PhantomData<T>,
-    pub node : (),
+    phantom_data : ::std::marker::PhantomData<T>,
+    pub node : Box<PromiseNode>,
 }
 
+pub trait Event {
+
+    // required methods
+
+    fn fire(&mut self);
+
+    // provided methods
+
+    fn arm_breadth_first(&mut self) {}
+    fn arm_depth_first(&mut self) {}
+}
+
+pub trait PromiseNode {
+    /// Arms the given event when ready.
+    fn on_ready(&self, event : &mut Event);
+
+    fn set_self_pointer(&mut self) {}
+    fn get(&self);
+}
+
+/// A promise that has already been resolved to an immediate value or error.
+pub struct ImmediatePromiseNode<T> {
+    result : ::std::result::Result<T, Error>,
+}
+
+impl <T> PromiseNode for ImmediatePromiseNode<T> {
+    fn on_ready(&self, event : &mut Event) {
+        event.arm_breadth_first();
+    }
+    fn get(&self) {
+
+    }
+}
 
