d2c9c8e8bdd30fbb817a0a1c96a0d6f42dfae212
David Renshaw <dwrenshaw@gmail.com>
2015-04-26 19:53:57 -0400

    EventLoop

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-
+#![allow(dead_code)]
 
 pub type Error = Box<::std::error::Error>;
 pub type Result<T> = ::std::result::Result<T, Error>;
@@ -9,15 +9,7 @@ pub struct Promise<T> {
 }
 
 pub trait Event {
-
-    // required methods
-
     fn fire(&mut self);
-
-    // provided methods
-
-    fn arm_breadth_first(&mut self) {}
-    fn arm_depth_first(&mut self) {}
 }
 
 pub trait PromiseNode {
@@ -35,10 +27,65 @@ pub struct ImmediatePromiseNode<T> {
 
 impl <T> PromiseNode for ImmediatePromiseNode<T> {
     fn on_ready(&self, event : &mut Event) {
-        event.arm_breadth_first();
+        unimplemented!();
+        //event.arm_breadth_first();
     }
     fn get(&self) {
 
     }
 }
 
+
+trait ErrorHandler {
+    fn task_failed(error: Error);
+}
+
+struct TaskSetImpl {
+    error_handler: Box<ErrorHandler>,
+}
+
+/// A Deque with an insertion point in the middle.
+struct InsertDeque<T> {
+    front: ::std::collections::VecDeque<T>,
+    back: ::std::collections::VecDeque<T>,
+}
+
+/// A queue of events being executed in a loop.
+struct EventLoop {
+    daemons: TaskSetImpl,
+    running: bool,
+    last_runnable_state: bool,
+    events: ::std::collections::VecDeque<Box<Event>>,
+    depth_first_events: ::std::collections::VecDeque<Box<Event>>,
+}
+
+impl EventLoop {
+
+    /// Run the even loop for `max_turn_count` turns or until there is nothing left to be done,
+    /// whichever comes first. This never calls the `EventPort`'s `sleep()` or `poll()`. It will
+    /// call the `EventPort`'s `set_runnable(false)` if the queue becomes empty.
+    pub fn run(&mut self, max_turn_count : u32) {
+        self.running = true;
+
+        for _ in 0..max_turn_count {
+            if !self.turn() {
+                break;
+            }
+        }
+    }
+
+    fn turn(&mut self) -> bool {
+        assert!(self.depth_first_events.is_empty());
+        match self.events.pop_front() {
+            None => return false,
+            Some(mut event) => {
+                // event->firing = true ?
+                event.fire();
+            }
+        }
+        while(!self.depth_first_events.is_empty()) {
+            self.events.push_front(self.depth_first_events.pop_back().unwrap());
+        }
+        return true;
+    }
+}
