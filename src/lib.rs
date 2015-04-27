74f22a0f2e8eca3f49373ea2e0c922ba7e7c0c58
David Renshaw <dwrenshaw@gmail.com>
2015-04-27 18:46:26 -0400

    tiny example nearly working

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ fn with_current_event_loop<F, R>(f: F) -> R
 pub type Error = Box<::std::error::Error>;
 pub type Result<T> = ::std::result::Result<T, Error>;
 
-pub struct Promise<T> {
+pub struct Promise<T> where T: 'static {
     pub node : Box<PromiseNode<T>>,
 }
 
@@ -30,13 +30,46 @@ impl <T> Promise <T> where T: 'static {
             Promise { node: node }
         }
 
-    pub fn wait(self) -> Result<T> {
-        unimplemented!()
+    pub fn wait(mut self) -> Result<T> {
+        with_current_event_loop(move |event_loop| {
+            let fired = ::std::rc::Rc::new(::std::cell::Cell::new(false));
+            let done_event = BoolEvent { fired: fired.clone()};
+            self.node.on_ready(Box::new(done_event));
+
+            event_loop.borrow_mut().running = true;
+
+            while !fired.get() {
+                if !event_loop.borrow_mut().turn() {
+                    // No events in the queue.
+                    panic!("need to implement EventPort");
+                }
+            }
+
+            self.node.get()
+        })
     }
 }
 
 pub trait Event {
     fn fire(&mut self);
+
+
+    /* TODO why doesn't this worked? Something about Sized?
+    fn arm_breadth_first(self: Box<Self>) {
+        with_current_event_loop(|event_loop| {
+     //       event_loop.borrow_mut().arm_breadth_first(self);
+        });
+    } */
+}
+
+struct BoolEvent {
+    fired: ::std::rc::Rc<::std::cell::Cell<bool>>,
+}
+
+impl Event for BoolEvent {
+    fn fire(&mut self) {
+        self.fired.set(true);
+    }
 }
 
 pub trait PromiseNode<T> {
@@ -110,12 +143,6 @@ struct TaskSetImpl {
     error_handler: Box<ErrorHandler>,
 }
 
-/// A Deque with an insertion point in the middle.
-struct InsertDeque<T> {
-    front: ::std::collections::VecDeque<T>,
-    back: ::std::collections::VecDeque<T>,
-}
-
 /// A queue of events being executed in a loop.
 pub struct EventLoop {
     daemons: TaskSetImpl,
@@ -135,7 +162,7 @@ impl EventLoop {
         self.events.push_back(event);
     }
 
-    /// Run the even loop for `max_turn_count` turns or until there is nothing left to be done,
+    /// Run the event loop for `max_turn_count` turns or until there is nothing left to be done,
     /// whichever comes first. This never calls the `EventPort`'s `sleep()` or `poll()`. It will
     /// call the `EventPort`'s `set_runnable(false)` if the queue becomes empty.
     pub fn run(&mut self, max_turn_count : u32) {
@@ -161,5 +188,134 @@ impl EventLoop {
             self.events.push_front(self.depth_first_events.pop_back().unwrap());
         }
         return true;
+    }
+}
+
+/// A callback which can be used to fulfill a promise.
+pub trait PromiseFulfiller<T> where T: 'static {
+    fn fulfill(&mut self, value: T);
+    fn reject(&mut self, error: Error);
+}
+
+pub enum OnReadyEvent {
+    Empty,
+    AlreadyReady,
+    Full(Box<Event>),
+}
+
+impl OnReadyEvent {
+    fn is_already_ready(&self) -> bool {
+        match self {
+            &OnReadyEvent::AlreadyReady => return true,
+            _ => return false,
+        }
+    }
+
+    fn is_empty(&self) -> bool {
+        match self {
+            &OnReadyEvent::Empty => return true,
+            _ => return false,
+        }
+    }
+
+    fn init(&mut self, new_event: Box<Event>) {
+        if self.is_already_ready() {
+            with_current_event_loop(|event_loop| {
+                event_loop.borrow_mut().arm_breadth_first(new_event);
+            });
+        } else {
+            *self = OnReadyEvent::Empty;
+        }
+    }
+
+    fn arm(&mut self) {
+        if self.is_empty() {
+            *self = OnReadyEvent::AlreadyReady;
+        } else {
+            let old_self = ::std::mem::replace(self, OnReadyEvent::Empty);
+            match old_self {
+                OnReadyEvent::Full(event) => {
+                    with_current_event_loop(|event_loop| {
+                        event_loop.borrow_mut().arm_depth_first(event);
+                    });
+                }
+                _ => {
+                    panic!("armed an event twice?");
+                }
+            }
+        }
+    }
+}
+
+pub struct PromiseAndFulfillerHub<T> where T: 'static {
+    result: Option<Result<T>>,
+    on_ready_event: OnReadyEvent,
+}
+
+impl <T> PromiseFulfiller<T> for PromiseAndFulfillerHub<T> where T: 'static {
+    fn fulfill(&mut self, value: T) {
+        if self.result.is_none() {
+            self.result = Some(Ok(value));
+        }
+    }
+
+    fn reject(&mut self, error: Error) {
+        if self.result.is_none() {
+            self.result = Some(Err(error));
+        }
+    }
+}
+
+/*
+pub struct WrapperPromiseNode<T> where T: 'static {
+    hub: ::std::rc::Rc<::std::cell::RefCell<PromiseAndFulfillerHub<T>>>,
+}
+*/
+
+impl <T> PromiseNode<T> for ::std::rc::Rc<::std::cell::RefCell<PromiseAndFulfillerHub<T>>> where T: 'static {
+    fn on_ready(&mut self, event: Box<Event>) {
+        self.borrow_mut().on_ready_event.init(event);
+    }
+    fn get(self: Box<Self>) -> Result<T> {
+        match ::std::mem::replace(&mut self.borrow_mut().result, None) {
+            None => panic!("no result!"),
+            Some(r) => r
+        }
+    }
+}
+
+impl <T> PromiseFulfiller<T> for ::std::rc::Rc<::std::cell::RefCell<PromiseAndFulfillerHub<T>>> where T: 'static {
+    fn fulfill(&mut self, value: T) {
+        self.borrow_mut().fulfill(value);
+    }
+
+    fn reject(&mut self, error: Error) {
+        self.borrow_mut().reject(error);
+    }
+}
+
+pub fn new_promise_and_fulfiller<T>() -> (Promise<T>, Box<PromiseFulfiller<T>>) where T: 'static {
+    let result = ::std::rc::Rc::new(::std::cell::RefCell::new(
+        PromiseAndFulfillerHub { result: None::<Result<T>>, on_ready_event: OnReadyEvent::Empty }));
+    let result_promise : Promise<T> = Promise { node: Box::new(result.clone())};
+    (result_promise, Box::new(result))
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn hello() {
+        let (mut promise, mut fulfiller) = ::new_promise_and_fulfiller::<u32>();
+        let p1 = promise.then(|x| {
+            println!("x = {}", x);
+            return Ok(x + 1);
+        }, |e| {
+            return Ok(1);
+        });
+
+        fulfiller.fulfill(10);
+        p1.wait();
+
     }
 }
